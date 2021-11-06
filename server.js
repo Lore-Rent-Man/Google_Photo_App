eb69346678a4d63cf0c19dae126b532de4db1305
Laurent Man <laurent.tian.man@gmail.com>
2021-11-06 14:32:32 -0400

    Added repsonse functionality as well as image recognition

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
+
 const http = require('http');
+const path = require('path');
 const express = require('express');
+const urlUtil = require('url');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
+const extName = require('ext-name');
+const { TextractClient, AnalyzeDocumentCommand } = require("@aws-sdk/client-textract");
+const client = new TextractClient({region: 'us-east-1'});
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
+    const { body } = req;
+    const { NumMedia, From: SenderNumber, MessageSid } = body;
+    const mediaItems = [];
 
-  twiml.message('The Robots are coming! Head for the hills!');
+    for (var i = 0; i < NumMedia; i++) {  // eslint-disable-line
+        const mediaUrl = body[`MediaUrl${i}`];
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+        const contentType = body[`MediaContentType${i}`];
+        const extension = extName.mime(contentType)[0].ext;
+    
+        if(extension === 'jpeg' || extension === 'png'){
+            const mediaSid = path.basename(urlUtil.parse(mediaUrl).pathname);
+            const filename = `${mediaSid}.${extension}`;
+
+            mediaItems.push({ mediaSid, MessageSid, mediaUrl, filename });
+        }
+    }
+
+    const messageBody = NumMedia === 0 ?
+    'Send us a sudoku puzzle!' :
+    `Thanks for sending us ${NumMedia} file(s)`;
+
+    const response = new MessagingResponse();
+    response.message({
+      from: '+13186071322',
+      to: SenderNumber,
+    }, messageBody);
+    
+    return res.send(response.toString()).status(200);
 });
 
 http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
+    console.log('Express server listening on port 1337');
 });
+
