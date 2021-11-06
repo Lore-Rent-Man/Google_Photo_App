98d453a056e5f34ac66e9724b1ea49102d1e18b2
Aaron <aaronbergfeld@gmail.com>
2021-11-06 17:26:35 -0400

    Add sudoku solving algo

---
+++ sudoku_algo_test.js
@@ -0,0 +1,17 @@
+const spawn = require("child_process").spawn;
+
+let puzzle = `[[5,3,0,0,7,0,0,0,0],
+              [6,0,0,1,9,5,0,0,0],
+              [0,9,8,0,0,0,0,6,0],
+              [8,0,0,0,6,0,0,0,3],
+              [4,0,0,8,0,3,0,0,1],
+              [7,0,0,0,2,0,0,0,6],
+              [0,6,0,0,0,0,2,8,0],
+              [0,0,0,4,1,9,0,0,5],
+              [0,0,0,0,8,0,0,7,9]]`
+
+const pythonProcess = spawn('python3',["./sudoku.py", puzzle]);
+
+pythonProcess.stdout.on('data', function(data) {
+    console.log(eval(data.toString()))
+})
